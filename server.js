a0184ebe2a9af1de2b57747fd5788dd581b624f0
sanathvobilisetty <sanathvobilisetty@outlook.com>
2016-10-02 20:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/completeprofile.html',function(req,res)
+app.get('/completeprofile',function(req,res)
 {
    res.sendFile(path.join(__dirname,'ui','completeprofile.html')); 
 });

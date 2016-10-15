625e2c91e33c792465364e363b4ac5a991b44581
sanathvobilisetty <sanathvobilisetty@outlook.com>
2016-10-15 20:35:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+var button=document.getElementbyId("clickme");
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechanged=function(){
+        if(request.readystate==XMLHttpRequest.DONE)
+        {
+            if(request.status==200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementbyId("span");
+                span.innerhtml=counter.toString();
+            }
+        }
+    };
+    request.open("http://sanathvobilisetty.imad.hasura-app.io/counter",null);
+    request.send(null);
+};

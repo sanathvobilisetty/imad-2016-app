0348acbe4f6bd8770260f5087a57348f26a985d0
sanathvobilisetty <sanathvobilisetty@outlook.com>
2016-10-15 20:39:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ var button=document.getElementbyId("clickme");
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechanged=function(){
-        if(request.readystate==XMLHttpRequest.DONE)
+        if(request.readystate===XMLHttpRequest.DONE)
         {
-            if(request.status==200)
+            if(request.status===200)
             {
                 var counter=request.responseText;
                 var span=document.getElementbyId("span");
